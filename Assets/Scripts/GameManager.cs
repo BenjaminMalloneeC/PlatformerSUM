384932ee1c914972b0854cac3cef4a5011a18e5f
Ben Mallonee <ben.mallonee.c@gmai.com>
2020-07-24 21:34:33 -0700

    Added Sounds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+    public int currentSceneIndex = 0;
+    public GameObject player;
+
+    public int points = 0;
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Debug.LogError("[GameManager] Attempteed to create a second isntance of GameManager");
+            Destroy(this.gameObject);
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LoadNextScene();
+        }
+    }
+
+    void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    /// <summary>
+    /// This method is called every time a scene finishes loading.
+    /// </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Scene finished loading");
+        currentSceneIndex = scene.buildIndex;
+    }
+
+    public void LoadNextScene()
+    {
+        LoadLevel(currentSceneIndex + 1);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,9 @@ public class Player : MonoBehaviour
 {
     public float speed = 3f;
     public float jumpForce = 5f;
+    public int currentJumps;
     public int maxJump = 1;
-    public float height = 0.2f;
+    public float height = 1f;
 
     private Rigidbody2D rigidBody;
     private SpriteRenderer sprite;
@@ -16,6 +17,7 @@ public class Player : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        currentJumps = maxJump;
     }
 
     // Update is called once per frame
@@ -36,18 +38,26 @@ public class Player : MonoBehaviour
 
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if (IsGrounded())
+            {
+                currentJumps = maxJump;
+            }
+            if (currentJumps > 0)
+            {
+                Jump();
+            }
         }
     }
 
     void Jump()
     {
+        currentJumps--;
         rigidBody.AddForce(Vector2.up * jumpForce);
     }
 
     bool IsGrounded()
     {
-        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, Vector2.down, (height / 2f + 0.1f));
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, Vector2.down, (height / 2f) + 0.1f);
 
         return (hitinfo.collider != null);
     }
